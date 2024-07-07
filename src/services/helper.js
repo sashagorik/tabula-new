f6ac9e1561e6289a729e4c3ae4ca20767c8d95de
sashagorik <sashagorik1982@gmail.com>
2024-07-07 15:41:49 +0300

    ркркркррк

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://tabula-new-1y88.vercel.app/";
+export const baseUrl = "https://tabula-new-1y88.vercel.app";
 export const frontUrl = "https://tabula-new-1y88.vercel.app/";
