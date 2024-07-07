9602116fa25c7c91603340f892849d68f0d108fd
sashagorik <sashagorik1982@gmail.com>
2024-07-07 14:07:49 +0300

    changes port

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ import cors from 'cors';
 //import { userInfo } from 'os';
 import helmet from 'helmet'; // Для управления заголовками безопасности, включая CSP
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
