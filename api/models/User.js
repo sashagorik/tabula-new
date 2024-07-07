65ec1d44800e54a53ccd27e322d7498190c16904
sashagorik <sashagorik1982@gmail.com>
2024-07-07 21:43:31 +0300

    delete required=true from models

--- api/models/User.js
+++ api/models/User.js
@@ -8,43 +8,43 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: true
+    
   },
   rank: {
     type: String,
-    required: true
+    
   },
   no_of_taps: {
     type: Number,
-    required: true
+    
   },
   total_coins: {
     type: Number,
-    required: true
+    
   },
   tap_coins: {
     type: Number,
-    required: true
+    
   },
   total_taps: {
     type: Number,
-    required: true
+    
   },
   flash_speed: {
     type: Number,
-    required: true
+    
   },
   recharge: {
     type: Number,
-    required: true
+    
   },
   turbo: {
     type: Number,
-    required: true
+    
   },
   allCoins: {
     type: Number,
-    required: true
+    
   },
 });
 
