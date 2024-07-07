e60fe59159884d8112fba6dbd1e18c3d91f0bc54
sashagorik <sashagorik1982@gmail.com>
2024-07-07 21:08:56 +0300

    vercel and mongo

--- api/index.mjs
+++ api/index.mjs
@@ -1,17 +1,22 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import User from './models/User.js'; // Импортируем модель пользователя
+import Booster from './models/Booster.js'; // Импортируем модель бустера
 import cors from 'cors';
-import helmet from 'helmet';
+//import { userInfo } from 'os';
+import helmet from 'helmet'; // Для управления заголовками безопасности, включая CSP
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 
 // Используем Helmet для добавления Content Security Policy
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-eval'"],
+    scriptSrc: ["'self'", "'unsafe-eval'"], // Разрешаем использование unsafe-eval, если необходимо
+    // Другие директивы, включая styleSrc, imgSrc, fontSrc, и т.д.
   }
 }));
 
@@ -22,6 +27,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
+  // Пропускать опции предзапросы
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
@@ -29,38 +35,28 @@ app.use((req, res, next) => {
   }
 });
 
-// Обработка JSON тела запроса
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
+// Обработчик GET запроса на корневой URL /
+app.get('/', (req, res) => {
+  res.send('Привет, мир!');
+});
 
+app.use(express.json()); // Парсинг JSON тела запроса
+app.use(cors()); // Разрешаем CORS для всех запросов
+app.use(bodyParser.json()); // Middleware для парсинга JSON
+
+// Подключение к базе данных MongoDB
 const uri = "mongodb+srv://sashagorik1982:bk8a2KDylgrENyI5@cluster0.6ire3pk.mongodb.net/?appName=Cluster0";
-let db;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri);
 
-async function connectToMongoDB() {
-  try {
-    await client.connect();
-    db = client.db();
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
 
-connectToMongoDB();
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
 
-// Middleware для проверки подключения к базе данных
-function checkDbConnection(req, res, next) {
-  if (!db) {
-    return res.status(500).json({ error: 'Database connection not established' });
-  }
-  next();
-}
-
-// Используем промежуточное ПО перед всеми запросами, требующими подключения к базе данных
-app.use(checkDbConnection);
-
+// Эндпоинт для получения данных пользователя
 app.get('/api/v1/userDetails', async (req, res) => {
   const { user_id } = req.query;
   if (!user_id) {
@@ -68,18 +64,20 @@ app.get('/api/v1/userDetails', async (req, res) => {
   }
 
   try {
-    const collection = db.collection('users');
-    const user = await collection.findOne({ user_id });
+    const user = await User.findOne({ user_id });
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
+    // Предположим, что ваша модель пользователя (User) имеет свойства user_id, name, total_coins и ton_coins
     const userData = {
       user_id: user.user_id,
       name: user.name,
       total_coins: user.total_coins,
       total_taps: user.total_taps
+      // ton_coins: user.ton_coins,
+      // Добавьте другие свойства пользователя по необходимости
     };
 
     res.json({ success: true, data: userData });
@@ -89,57 +87,71 @@ app.get('/api/v1/userDetails', async (req, res) => {
   }
 });
 
+// Эндпоинт для сохранения данных пользователя
 app.post('/api/v1/userDetails', async (req, res) => {
   const userData = req.body;
 
   try {
-    const collection = db.collection('users');
-    let user = await collection.findOne({ user_id: userData.user_id });
+    let user = await User.findOne({ user_id: userData.user_id });
 
     if (!user) {
-      await collection.insertOne(userData);
-      await db.collection('boosters').insertOne({
+      // Если пользователь не найден, создаем нового
+      user = new User({
+        user_id: userData.user_id,
+        name: userData.name,
+        total_coins: userData.total_coins,
+        total_taps: userData.total_taps
+      });
+      await user.save();
+
+      // После создания пользователя также создаем запись о бустерах
+      const booster = new Booster({
         user_id: userData.user_id,
         multiTap: 0,
         fireLimit: 0,
         flashSpeed: 0,
         hireAnt: false,
-        recharge: 3,
-        turbo: 3
+        recharge: 3, // Пример начального значения для recharge
+        turbo: 3 // Пример начального значения для turbo
       });
+      await booster.save();
     } else {
-      await collection.updateOne({ user_id: userData.user_id }, { $set: userData });
+      // Обновляем данные пользователя, если он уже существует
+      user = Object.assign(user, userData);
+      await user.save();
     }
 
-    res.json({ success: true, data: userData });
+    res.json({ success: true, data: user });
   } catch (err) {
     console.error('Error saving user data:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
+
+// Эндпоинт для обновления количества монет totalcoins
 app.post('/api/v1/updateCoins', async (req, res) => {
   const { user_id, coins } = req.body;
 
   try {
-    const collection = db.collection('users');
-    let user = await collection.findOne({ user_id });
+    let user = await User.findOne({ user_id });
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
+    // Обновляем количество монет
     user.total_coins += coins;
 
-    await collection.updateOne({ user_id }, { $set: { total_coins: user.total_coins } });
-
+    await user.save();
     res.json({ success: true, data: user });
   } catch (err) {
-    console.error('Error updating coins:', err);
+    console.error('Error updating coins:', err); // Выводим ошибку в консоль
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
+// Эндпоинт для получения данных о бустерах
 app.get('/api/v1/boosterDetails', async (req, res) => {
   const { user_id } = req.query;
   if (!user_id) {
@@ -147,13 +159,10 @@ app.get('/api/v1/boosterDetails', async (req, res) => {
   }
 
   try {
-    const collection = db.collection('boosters');
-    const booster = await collection.findOne({ user_id });
-
+    const booster = await Booster.findOne({ user_id });
     if (!booster) {
       return res.status(404).json({ error: 'Booster data not found' });
     }
-
     res.json({ success: true, data: booster });
   } catch (err) {
     console.error('Error fetching booster data:', err);
@@ -161,21 +170,20 @@ app.get('/api/v1/boosterDetails', async (req, res) => {
   }
 });
 
+// Эндпоинт для обновления данных о бустерах
 app.post('/api/v1/updateBooster', async (req, res) => {
   const { user_id, boosterData } = req.body;
 
   try {
-    const collection = db.collection('boosters');
-    let booster = await collection.findOne({ user_id });
-
+    let booster = await Booster.findOne({ user_id });
     if (!booster) {
       return res.status(404).json({ error: 'Booster data not found' });
     }
 
+    // Обновляем данные о бустерах
     booster = Object.assign(booster, boosterData);
 
-    await collection.updateOne({ user_id }, { $set: booster });
-
+    await booster.save();
     res.json({ success: true, data: booster });
   } catch (err) {
     console.error('Error updating booster data:', err);
@@ -183,15 +191,14 @@ app.post('/api/v1/updateBooster', async (req, res) => {
   }
 });
 
+
+// Эндпоинт для апгрейда бустера
 app.post('/api/v1/upgradeBooster', async (req, res) => {
   const { user_id, boosterType } = req.body;
 
   try {
-    const usersCollection = db.collection('users');
-    const boostersCollection = db.collection('boosters');
-
-    let user = await usersCollection.findOne({ user_id });
-    let booster = await boostersCollection.findOne({ user_id });
+    let user = await User.findOne({ user_id });
+    let booster = await Booster.findOne({ user_id });
 
     if (!user || !booster) {
       return res.status(404).json({ error: 'User or Booster data not found' });
@@ -220,12 +227,13 @@ app.post('/api/v1/upgradeBooster', async (req, res) => {
     user.total_coins -= price;
     booster[boosterType] += 1;
 
+    // Если апгрейдируем fireLimit, увеличиваем total_taps
     if (boosterType === 'fireLimit') {
-      user.total_taps += 100;
+      user.total_taps += 100; // Добавляем 100 очков за каждый уровень fireLimit
     }
 
-    await usersCollection.updateOne({ user_id }, { $set: user });
-    await boostersCollection.updateOne({ user_id }, { $set: booster });
+    await user.save();
+    await booster.save();
 
     res.json({ success: true, data: { user, booster } });
   } catch (err) {
@@ -234,29 +242,89 @@ app.post('/api/v1/upgradeBooster', async (req, res) => {
   }
 });
 
-app.post('/api/v1/dailyReward', async (req, res) => {
-  const { user_id } = req.body;
+
+// Эндпоинт для получения уровня бустера пользователя
+app.get('/api/v1/boosterLevel', async (req, res) => {
+  const { user_id, booster } = req.query;
+  if (!user_id || !booster) {
+    return res.status(400).json({ error: 'Missing user_id or booster parameter' });
+  }
 
   try {
-    const collection = db.collection('users');
-    let user = await collection.findOne({ user_id });
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    const boosterData = await Booster.findOne({ user_id });
+    if (!boosterData) {
+      return res.status(404).json({ error: 'Booster data not found' });
     }
-
-    const reward = 100;
-    user.total_coins += reward;
-
-    await collection.updateOne({ user_id }, { $set: { total_coins: user.total_coins } });
-
-    res.json({ success: true, data: user });
+    const boosterLevel = boosterData[booster];
+    if (boosterLevel === undefined) {
+      return res.status(400).json({ error: 'Invalid booster type' });
+    }
+    res.json({ success: true, level: boosterLevel });
   } catch (err) {
-    console.error('Error processing daily reward:', err);
+    console.error('Error fetching booster level:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+// Эндпоинт Обновление total_taps пользователя
+app.post('/api/v1/updateTotalTaps', async (req, res) => {
+  const { user_id, total_taps } = req.body;
+  try {
+    const updatedUser = await User.findOneAndUpdate({ user_id }, { total_taps }, { new: true });
+    res.json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.error('Error updating total taps:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
+// Получение данных о ежедневных вознаграждениях
+app.get('/api/dailyRewardDetails/:userId', async (req, res) => {
+  try {
+      const user = await User.findOne({ user_id: req.params.userId });
+      if (!user) {
+          return res.status(404).send('User not found');
+      }
+      res.json(user.dailyRewards);
+  } catch (error) {
+      res.status(500).send(error);
+  }
+});
+
+// Обновление данных о ежедневных вознаграждениях
+app.post('/api/updateDailyReward/:userId', async (req, res) => {
+  try {
+      const user = await User.findOne({ user_id: req.params.userId });
+      if (!user) {
+          return res.status(404).send('User not found');
+      }
+
+      const today = new Date();
+      const lastClaimDate = new Date(user.dailyRewards.lastClaimDate);
+      const isSameDay = today.getDate() === lastClaimDate.getDate() &&
+                        today.getMonth() === lastClaimDate.getMonth() &&
+                        today.getFullYear() === lastClaimDate.getFullYear();
+
+      if (!isSameDay) {
+          user.dailyRewards.claimDays += 1;
+          user.dailyRewards.isClaimed = false;
+      }
+
+      user.dailyRewards.lastClaimDate = today;
+      user.total_coins += req.body.coins;
+      user.dailyRewards.isClaimed = req.body.isClaimed;
+
+      await user.save();
+      res.json(user.dailyRewards);
+  } catch (error) {
+      res.status(500).send(error);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
