4eff7370ee9088e4467c3f23e140f770a63fea83
sashagorik <sashagorik1982@gmail.com>
2024-07-07 23:59:43 +0300

    App.jsx and Home.jsx userid

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import React from 'react'
-import './App.css'
-import Home from './Pages/Home'
-import { Routes, Route } from "react-router-dom"
-import Layout from './Layout/Layout'
+import React, { useContext, useEffect, useState } from 'react';
+import './App.css';
+import Home from './Pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Layout from './Layout/Layout';
 import Earn from './Pages/Earn/Earn';
 import Invite from './Pages/Invite/Invite';
 import Booster from './Pages/Booster/Booster';
 import Task from './Pages/Task/Task';
-
 import { baseUrl } from './services/helper';
-//import { io } from 'socket.io-client'
-import { useContext, useEffect, useState } from 'react';
+// import { io } from 'socket.io-client';
 import { UserInfo } from './ContextApi/UserData';
 import BoosterData from './ContextApi/BoosterData';
 import Loader from './components/Loader/Loader';
 import SocialContext from './ContextApi/SocialContext';
 import Rank from './Pages/Rank/Rank';
 
-
-
-
-
 function App() {
-
-
-  const [loader, setloadder] = useState(false)
-  const { userInfo, setUserInfo } = useContext(UserInfo)
+  const [loader, setLoader] = useState(false);
+  const { userInfo, setUserInfo } = useContext(UserInfo);
+  const [user_id, setUser_id] = useState("");
 
   useEffect(() => {
-    setloadder(true)
+    setLoader(true);
     setTimeout(() => {
-      setloadder(false)
-    }, 2000)
-  }, [])
+      setLoader(false);
+    }, 2000);
+  }, []);
 
-
-  // to update the progress bar if user is at home page or not
+  // Обновление прогресса каждый раз, когда пользователь на главной странице
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (userInfo.used_taps < userInfo.total_taps) {
@@ -50,61 +42,7 @@ function App() {
     }, 2000); 
 
     return () => clearInterval(intervalId);
-  }, [userInfo]);
-
-
-  // connecting with socket.io
-  const [isSocket, setSocket] = useState(null)
-  const [user_id, setUser_id] = useState("")
-  const [antHire, setAntHire] = useState(false)
-  // useEffect(() => {
-
-  //   const searchParams = new URLSearchParams(window.location.hash.substring(1));
-  //   const tgWebAppData = searchParams.get("tgWebAppData");
-  //   if (tgWebAppData) {
-  //     const userParam = new URLSearchParams(tgWebAppData).get("user");
-  //     if (userParam) {
-  //       // Decode the user parameter
-  //       const decodedUserParam = decodeURIComponent(userParam);
-  //       // Parse the JSON to extract the user ID
-  //       const userObject = JSON.parse(decodedUserParam);
-  //       const userId = userObject.id;
-  //       setUser_id(userId)
-  //     }
-  //   } else {
-  //     // when user came from another page so we need to maintaina and save the id
-  //     if (localStorage.getItem("user_id")) {
-  //       setUser_id(localStorage.getItem("user_id"))
-  //     }
-  //   }
-  //   console.log("join", user_id)
-
-
-
-  //   setTimeout(() => {
-  //     if (user_id) {
-  //       // const socket = io.connect(`${baseUrl}`, { transports: ["websocket"] });
-  //       const socket = io.connect("ws://192.168.29.43:5006", { transports: ["websocket"] });
-  //       console.log(user_id)
-  //       // making connection for hire ant
-  //       socket.on('connect', () => {
-  //         console.log(`Connected with socket ID: ${socket.id}`);
-  //         socket.emit('join', { user_id: user_id });
-  //       });
-  //       setSocket(socket)
-  //     }
-  //   }, 1000)
-  //   // console.log("socket", socket)
-  // }, [])
-
-
-
-
-
-  // to get the user id and setting the user_id
-
-
-
+  }, [userInfo, setUserInfo]);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.hash.substring(1));
@@ -112,16 +50,13 @@ function App() {
     if (tgWebAppData) {
       const userParam = new URLSearchParams(tgWebAppData).get("user");
       if (userParam) {
-        // Decode the user parameter
         const decodedUserParam = decodeURIComponent(userParam);
-        // Parse the JSON to extract the user ID
         const userObject = JSON.parse(decodedUserParam);
         const userId = userObject.id;
         setUser_id(userId);
         localStorage.setItem("user_id", userId);
       }
     } else {
-      // when user came from another page so we need to maintain and save the id
       const savedUserId = localStorage.getItem("user_id");
       if (savedUserId) {
         setUser_id(savedUserId);
@@ -129,88 +64,48 @@ function App() {
     }
   }, []);
 
-
-
-  // for ocket.connection and sending id 
-  //useEffect(() => {
-    //if (user_id) {
-      // const socket = io.connect("ws://192.168.29.43:5006", { transports: ["websocket"] });
-      //const socket = io.connect(`${baseUrl}`, { transports: ["websocket"] });
-      //socket.on('connect', () => {
-       // console.log(`Connected with socket ID: ${socket.id}`);
-       // socket.emit('join', { user_id });
-     // });
-     // setSocket(socket);
-
-      // getting details about ant hire
-     // socket.on("response", (data) => {
-     //   setAntHire(data.antHire)
-     // })
-
-
-
-
-
-      // Clean up on component unmount
-   //   return () => {
-  //      socket.disconnect();
-   //   };
-  //  }
- // }, []);
-
-
-
-  // const updateHireAntCoin = () => {
-  //   console.log("anthire",antHire)
-  //   if (antHire) {
-  //     isSocket.emit("hireAnt", `${user_id}, ${1}` );
-
-  //     let updated_coins = userInfo.hireAntCoins + 1 
-  //     // setUserInfo((prevUserInfo) => ({...prevUserInfo, total_coins: updated_coins }));
-  //     setUserInfo((prevUserInfo) => ({...prevUserInfo, hireAntCoins: updated_coins }));
-  //     // console.log(userInfo)
-  //     setTimeout(() => {
-  //       updateHireAntCoin()
-  //     }, 3000)
-  //     console.log("hitted")
-  //   }
-  // }
-
+  // Подключение к socket.io и отправка user_id
   // useEffect(() => {
-  //   updateHireAntCoin()
-  // }, [antHire]);
+  //   if (user_id) {
+  //     const socket = io.connect(`${baseUrl}`, { transports: ["websocket"] });
+  //     socket.on('connect', () => {
+  //       console.log(`Connected with socket ID: ${socket.id}`);
+  //       socket.emit('join', { user_id });
+  //     });
+  //     setSocket(socket);
 
+  //     socket.on("response", (data) => {
+  //       setAntHire(data.antHire);
+  //     });
 
-
-
+  //     return () => {
+  //       socket.disconnect();
+  //     };
+  //   }
+  // }, [user_id]);
 
   return (
     <>
-
-      {
-        loader ?
-          <Loader />
-          :
-
-          <SocialContext>
-            <BoosterData>
-              <Routes>
-                <Route path='' element={<Layout />} >
-                  <Route path="/" element={<Home socket={isSocket} />} />
-                   <Route path="/task" element={<Task />} />
-                  <Route path="/boost" element={<Booster />} />
-                  <Route path="/invite" element={<Invite />} />
-                  <Route path="/earn" element={<Earn />} />
-      <Route path="/rank" element={<Rank />} />
-                </Route>
-              </Routes>
-            </BoosterData>
-          </SocialContext>
-
-      }
-
+      {loader ? (
+        <Loader />
+      ) : (
+        <SocialContext>
+          <BoosterData>
+            <Routes>
+              <Route path='' element={<Layout />} >
+                <Route path="/" element={<Home socket={isSocket} />} />
+                <Route path="/task" element={<Task />} />
+                <Route path="/boost" element={<Booster />} />
+                <Route path="/invite" element={<Invite />} />
+                <Route path="/earn" element={<Earn />} />
+                <Route path="/rank" element={<Rank />} />
+              </Route>
+            </Routes>
+          </BoosterData>
+        </SocialContext>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
