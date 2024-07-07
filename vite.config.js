a4a6b237e7e67e392ab03ab71e5e9a825f57fb60
sashagorik <sashagorik1982@gmail.com>
2024-07-07 13:10:47 +0300

    server side commit

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
       external: ['firebase/app'], // явно исключаем Firebase
     },
   },
+  server: {
+    port: 3000,       // Укажите порт для dev server
+    open: true,       // Автоматически открывать браузер при запуске dev server
+    host: true,       // Делает сервер доступным через локальную сеть
+  },
 });
