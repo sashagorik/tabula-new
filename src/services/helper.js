f2d45f531ab6f35086fd5e523c3882804f443ae7
sashagorik <sashagorik1982@gmail.com>
2024-07-07 14:41:50 +0300

    server tune

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,2 +1,2 @@
 export const baseUrl = "https://tabula-new-1y88.vercel.app";
-export const frontUrl = "https://tabula-new-1y88-git-main-alessandros-projects-f7ed6286.vercel.app";
+export const frontUrl = "https://tabula-new-1y88.vercel.app";
