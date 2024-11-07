0fd529ada468cc5ed06257a239e3023e8b52a0ae
OleksandrHorilenko <a.gorilenko82@icloud.com>
2024-11-07 23:31:51 +0200

    07112024 2331

--- api/models/Booster.js
+++ api/models/Booster.js
@@ -2,12 +2,10 @@ import mongoose from 'mongoose';
 
 const boosterSchema = new mongoose.Schema({
   user_id: { type: String, required: true, unique: true },
-  multiTap: { type: Number, default: 1 },
-  fireLimit: { type: Number, default: 1 },
-  flashSpeed: { type: Number, default: 1 },
-  hireAnt: { type: Boolean, default: false },
-  recharge: { type: Number, default: 3 },
-  turbo: { type: Number, default: 3 }
+  profitPerHour: {
+    type: Number,
+    required: true
+  },
 });
 
 const Booster = mongoose.model('Booster', boosterSchema);
--- api/models/User.js
+++ api/models/User.js
@@ -6,62 +6,23 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  name: {
-    type: String,
-    required: true
-  },
-  rank: {
-    type: String,
-    required: true
-  },
-  no_of_taps: {
-    type: Number,
-    required: true
-  },
-  total_coins: {
-    type: Number,
-    required: true
-  },
-  tap_coins: {
-    type: Number,
-    required: true
-  },
-  total_taps: {
-    type: Number,
-    required: true
-  },
-  flash_speed: {
-    type: Number,
-    required: true
-  },
-  recharge: {
-    type: Number,
-    required: true
-  },
-  turbo: {
-    type: Number,
-    required: true
-  },
-  allCoins: {
-    type: Number,
-    required: true
-  },
+ 
   profitPerHour: {
     type: Number,
     required: true
   },
 
   // Новые поля для даты и времени
-  registrationDate: {
-    type: Date,
-    default: Date.now,  // Устанавливаем текущую дату при регистрации
-    required: true
-  },
-  lastLoginDate: {
-    type: Date,
-    default: Date.now,  // Инициализация последнего входа при создании
-    required: true
-  }
+ // registrationDate: {
+  //  type: Date,
+  //  default: Date.now,  // Устанавливаем текущую дату при регистрации
+  //  required: true
+ // },
+  //lastLoginDate: {
+  //  type: Date,
+  //  default: Date.now,  // Инициализация последнего входа при создании
+  //  required: true
+ // }
 });
 
 // Обновляем `lastLoginDate` каждый раз, когда пользователь входит в систему
